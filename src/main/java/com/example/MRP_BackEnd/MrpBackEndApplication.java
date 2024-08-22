84943511890ee989eb9244d585b14c1d753a7d90
EricBritto14 <Eric.Britto22@gmail.com>
2024-08-22 16:47:28 -0300

    Inicio do projeto

---
+++ src/main/java/com/example/MRP_BackEnd/MrpBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.MRP_BackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MRP_BackEnd/MrpBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MRP_BackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrpBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
